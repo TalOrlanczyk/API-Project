c73146189c8f6d2bd33291c3bce73f9053b0aa21
TalOrlanczyk <tal.orlanczyk@gmail.com>
2020-08-29 18:02:11 +0300

    remove spotify

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './MainPage.css';
 import CovertLink from './CovertLink/CovertLink';
-import { Link } from 'react-router-dom';
 import SpaceXLink from './SpaceXLink/SpaceXLink';
 
 const MainPage = () => {
