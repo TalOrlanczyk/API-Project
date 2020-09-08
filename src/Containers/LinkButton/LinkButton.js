c0d6a9de2e7baee3e0ff8a43d713316239335a41
TalOrlanczyk <tal.orlanczyk@gmail.com>
2020-09-09 00:14:29 +0300

    let csss change to LinkButton

--- src/Containers/LinkButton/LinkButton.js
+++ src/Containers/LinkButton/LinkButton.js
@@ -2,22 +2,22 @@ import React from 'react';
 import styles from './LinkButton.module.css'
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import clsx from 'clsx';
 const style = (theme) => {
       const {backgroundColor,color,size, borderRadius} =  theme || {}
     
-    return { 
-        backgroundColor: backgroundColor ? backgroundColor: styles.Background,
-        color: color ? color : styles.Color,
-        size: size ? size : styles.Size,
-        borderRadius: borderRadius ? borderRadius : styles.borderRadius,
-    };
+    return [
+        backgroundColor ? backgroundColor: styles.Background,
+        color ? color : styles.Color,
+        size ? size : styles.Size,
+        borderRadius ? borderRadius : styles.borderRadius,
+        styles.Link
+    ].join(" ");
   };
 const LinkButton = ({ to = "", classes, children}) => {
     const theme = style(classes);
     return (
         <div className = {styles.Wrappper}>
-            <Link to={to} className={clsx(theme.color,theme.size,theme.backgroundColor,theme.borderRadius)}>
+            <Link to={to} className={theme}>
                 {children}
             </Link>
         </div>
